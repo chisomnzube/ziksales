14cceb7d815dc912d6d4780b090a6e58b4c3a931
Chisom Nzube <chisomnzube@gmail.com>
2020-12-02 09:59:41 +0100

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,25 @@
             
               gtag('config', 'UA-130773828-1');
             </script>
+            <!-- Facebook Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+             fbq('init', '286325569353767'); 
+            fbq('track', 'PageView');
+            </script>
+            <noscript>
+             <img height="1" width="1" 
+            src="https://www.facebook.com/tr?id=286325569353767&ev=PageView
+            &noscript=1"/>
+            </noscript>
+            <!-- End Facebook Pixel Code -->
             {{--<link rel="shortcut icon" type="image/icon" href="{{ asset('img/blog/ziksaleslogo.png') }}">--}}
             <meta name="theme-color" content="#ff6600">
             <link rel='shortcut icon' type='image/ico' href='{{ asset('img/blog/ziksaleslogo.ico') }}' /> 
