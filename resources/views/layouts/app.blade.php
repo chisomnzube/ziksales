81a3c8fe1599b9396919dd5f60db715b75b619e4
Chisom Nzube <chisomnzube@gmail.com>
2020-12-02 10:15:02 +0100

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,12 +23,12 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window,document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-             fbq('init', '286325569353767'); 
+             fbq('init', '2312211849087646'); 
             fbq('track', 'PageView');
             </script>
             <noscript>
              <img height="1" width="1" 
-            src="https://www.facebook.com/tr?id=286325569353767&ev=PageView
+            src="https://www.facebook.com/tr?id=2312211849087646&ev=PageView
             &noscript=1"/>
             </noscript>
             <!-- End Facebook Pixel Code -->
